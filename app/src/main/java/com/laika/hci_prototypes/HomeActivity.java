71803fbee524cf2d4ef96d65a60dfe9656b46684
Wing <wyy3@students.waikato.ac.nz>
2018-10-11 21:12:22 +1300

    bottom navigation bar completed

---
+++ app/src/main/java/com/laika/hci_prototypes/HomeActivity.java
@@ -0,0 +1,56 @@
+package com.laika.hci_prototypes;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class HomeActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+
+        loadFragment(new HomeFragment());
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if(fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container,fragment)
+                    .commit();
+
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+        switch (menuItem.getItemId()) {
+            case R.id.navigation_home:
+                fragment = new HomeFragment();
+                break;
+            case R.id.navigation_messages:
+                fragment = new MessagesFragment();
+                break;
+            case R.id.navigation_network:
+                fragment = new NetworkFragment();
+                break;
+            case R.id.navigation_jobs:
+                fragment = new JobsFragment();
+                break;
+        }
+
+        return loadFragment(fragment);
+    }
+}
